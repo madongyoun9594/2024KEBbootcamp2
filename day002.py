1adf4ed912ab3717c44c4caf8ac016f906b149f7
madongyon9594 <madongyoun9594@gmail.com>
2024-01-15 15:43:18 +0900

    v1.2 python day02

--- day002.py
+++ day002.py
@@ -1,7 +1,4 @@
-
 # (100°F − 32) × 5/9 = 37.778°C
-fahrenheit = float(input('Input Fahrenheit : '))
-print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
 # (0°C × 9/5) + 32 = 32°F
 
 menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
@@ -11,5 +8,14 @@ if menu == '1':
     print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
 elif menu == '2':
     celsius = float(input('Input Celcius : '))
+    celsius = float(input('Input Celsius : '))
     print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+else:
+    print('Terminate Program.')
 
+# temp = [0]
+# temp = []
+# if temp:
+#     print("원소가 존재하는 리스트")
+# else:
+#     print("비어있는 리스트")
