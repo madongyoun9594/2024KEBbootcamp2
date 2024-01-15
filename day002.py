34b3044a99a96b27dfe2a12f38d046bc46ffbff9
madongyon9594 <madongyoun9594@gmail.com>
2024-01-15 13:22:00 +0900

    v7 python day 02

--- day002.py
+++ day002.py
@@ -1,8 +1,20 @@
-artists = ['BTS', 'SES', 'HOT', '뉴진스']
-groups = artists
-artists[2] = '세븐틴'
-print(artists, groups)
-print(bool(0))
-print(+123)
+base_number = int(input('Input base number : '))
+exponent_number = int(input('Input exponent  number : '))
+print(type(base_number),type(exponent_number))
+# print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {base_number**exponent_number}')
+print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {pow(base_number,exponent_number)}')
+
+# c like
+print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number,exponent_number))),
 
 
+# money = 5,000,000
+# print(money)
+# # print(type(money)) # tuple
+# cash = 5_000_000
+# print(cash)
+# print(type(cash)) # int
+#
+# print( 7 / 2)
+# print( 7 // 2)
+# print( 3 ** 4)
