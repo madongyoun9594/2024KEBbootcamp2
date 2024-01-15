6a596155cad94e7ecd59b94b3a4f88141615ee9f
madongyon9594 <madongyoun9594@gmail.com>
2024-01-15 14:15:36 +0900

    v0.9 python day02

--- day002.py
+++ day002.py
@@ -1,14 +1,15 @@
-a = 100
-a = a + 10
-print(a)
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(binary), chr(dec), chr(octal), chr(hexadecimal))
 
-a = 100
-a = a // 10
-print(a)
-
-first_number = int(input("First number : "))
-second_number = int(input("Second number : "))
-
-quotient = first_number // second_number
-remainder = first_number % second_number
-print(f'몫은 {quotient} 나머지는 {remainder}입니다')
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
+print(ord('B'), ord('Z'), ord('a'), ord('2'))  # 66, 90, 97, 50
