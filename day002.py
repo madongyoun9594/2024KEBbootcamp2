f12b8eec40c9dba81b404f85a916792833ad5a44
madongyon9594 <madongyoun9594@gmail.com>
2024-01-15 14:29:57 +0900

    v1.0 pyhton day02

--- day002.py
+++ day002.py
@@ -1,15 +1,2 @@
-dec = 65
-octal = 0o101
-hexadecimal = 0x41
-binary = 0b01000001
-print(dec, octal, hexadecimal, binary)
-print(chr(binary), chr(dec), chr(octal), chr(hexadecimal))
-
-dec = 65
-octal = 0o101
-hexadecimal = 0x41
-binary = 0b01000001
-print(dec, octal, hexadecimal, binary)
-print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
-print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
-print(ord('B'), ord('Z'), ord('a'), ord('2'))  # 66, 90, 97, 50
+fahrenheit = float(input('input Fahrenheit : '))
+print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5/9):.2f}C')
