1d27e646cc702489134db5a527c65b425212cc8e
madongyon9594 <madongyoun9594@gmail.com>
2024-01-15 13:44:26 +0900

    v0.8 python day02

--- day002.py
+++ day002.py
@@ -1,20 +1,14 @@
-base_number = int(input('Input base number : '))
-exponent_number = int(input('Input exponent  number : '))
-print(type(base_number),type(exponent_number))
-# print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {base_number**exponent_number}')
-print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {pow(base_number,exponent_number)}')
+a = 100
+a = a + 10
+print(a)
 
-# c like
-print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number,exponent_number))),
+a = 100
+a = a // 10
+print(a)
 
+first_number = int(input("First number : "))
+second_number = int(input("Second number : "))
 
-# money = 5,000,000
-# print(money)
-# # print(type(money)) # tuple
-# cash = 5_000_000
-# print(cash)
-# print(type(cash)) # int
-#
-# print( 7 / 2)
-# print( 7 // 2)
-# print( 3 ** 4)
+quotient = first_number // second_number
+remainder = first_number % second_number
+print(f'몫은 {quotient} 나머지는 {remainder}입니다')
