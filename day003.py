f04387a2a8a7fb58ba07ce5d8e1539c908db4076
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-16 14:59:31 +0900

    v2.7 pyhton day 003

--- day003.py
+++ day003.py
@@ -1,31 +1,17 @@
-# prime number
-number = int(input('Enter a number: '))
-is_prime = True
+univ = 'inha'
+i = 0
+while i < len(univ):
+    print(univ[i], end=' ')
+    i = i + 1
 
+print()
 
-if number < 2:
-    print(f'{number} is Not a prime number')
-else:
-    i = 2
-    while i < number:
-        if number % i == 0:
-            is_prime = False
-            break
-        i = i + 1
-    # print(i, end='')
+for letter in univ:
+    print(letter, end=' ')
 
-# if cnt == 0:
-if is_prime:  # remove ==
-            print(f'{number} is prime number')
-else:
-            print(f'{number} is NOT prime number')
+print()
 
-
-# subjects = {'python': 'kim', 'c++': 'sung', 'datastructures': 'kim', 'database': 'kang'}
-# print("{0[python]} {0[datastructures]}".format(subjects))
-# while True:
-#     stuff = input("String to capitalize [type q to quit]: ")
-#     if stuff == 'q':
-#         break
-#     print(stuff.capitalize())
+# for k in range(0, len(univ), 1):
+for k in range(len(univ)):
+    print(univ[k], end=' ')
 
