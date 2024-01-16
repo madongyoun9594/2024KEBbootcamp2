518d9d5ee718b8a7667e59feea9afb202654e29d
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-16 12:42:05 +0900

    v1.9 pyhton day 003

--- day003.py
+++ day003.py
@@ -1,16 +1,29 @@
-subjects = ["pyton", "python", "java", "javascript"]
-subjects_string = ' / ' . join(subjects)
-print(subjects_string)
-
-numbers = input("FirstNumber SecondNumber: ").split()
-# print(numbers[0] + numbers[1]) # concatenation
-print(int(numbers[0]) + int(numbers[1])) # arithmetic operation
 
 
-# course = "2024 KEB bootcamp"
+
+course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
+print(course.find('KEB'))
+print(course.rfind('KEB'))
+print(course.index('KEB'))
+print(course.rindex('KEB'))
+print(course.find('Inha'))  # -1
+print(course.index('Inha'))  # ValueError: substring not found
+
+print(course)
+course = course.replace('KEB', 'Inha', 2)
+print(course)
+print(course.strip())
+print(course.strip("!#.*"))
+
+
 # print(course)
-# # list_c = course.split(" ")
-# list_course = course.split('B')
-# print(list_course)
-
-
+# print(course.replace('KEB', 'Inha'))
+# print(course)
+# course = course.replace('KEB', 'Inha')
+# print(course)
+subjects = "python c++ database linux"
+subject = input("수강신청과목 입력 : ")
+if subjects.find(subject) != -1:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.")
+else:
+    print('해당과목이 존재하지 않습니다')
