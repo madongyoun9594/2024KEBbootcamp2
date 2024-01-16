52f05f2faf22be64f7dbb4a10fea6938698c3a00
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-16 14:06:15 +0900

    v2.3 prime number pyhton day 003

--- day003.py
+++ day003.py
@@ -1,13 +1,13 @@
 # prime number
 number = int(input('Enter a number: '))
 cnt =0
-i = 1
-while i<= number:
+i = 2
+while i < number:
     if number % i == 0:
         cnt = cnt + 1
     i = i + 1
 
-if cnt == 2:
+if cnt == 0:
             print(f'{number} is prime number')
 else:
             print(f'{number} is NOT prime number')
