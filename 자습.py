c080092459c06d23fb1bd3ea6a66b19c9237523d
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-17 18:47:21 +0900

    v0.03 day003

--- 자습.py
+++ 자습.py
@@ -1,11 +1,17 @@
 
+e2f = dict(dog='chien', cat='chat', walrus='mors')
+f2e = {}
+for (영어, 프랑스어) in e2f.items():
+    f2e[프랑스어] = 영어
 while True:
-    menu = input( '1) e2f dict 2) quit : ')
+    menu = input( '1) e2f dict  2) f2e dict  3) quit : ')
     if menu == '1':
         E = input('영어: ')
-        e2f = dict(dog='chien', cat='chat', walrus='mors')
         print(f'프랑스어: {e2f.get(E)} ')
     elif menu == '2':
+        F = input('프랑스어: ')
+        print(f'영어: {f2e.get(F)} ')
+    elif menu == '3':
         print("종료")
         break
     else:
