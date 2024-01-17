e4874db69ebd91a858aca6a321770fe26db9a96f
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-17 17:53:17 +0900

    v0.02 day003

--- 자습.py
+++ 자습.py
@@ -1,12 +1,13 @@
-number = int(input('Enter a number: '))
 
-i = 2
-while i < number:
-        if number % i == 0:
-            print(f'The number {number} is not prime')
-            break
-        i += 1
-if i == number:
-    print(f'The number {number} is prime')
-else:
-    print(f'The number {number} is not prime')
+while True:
+    menu = input( '1) e2f dict 2) quit : ')
+    if menu == '1':
+        E = input('영어: ')
+        e2f = dict(dog='chien', cat='chat', walrus='mors')
+        print(f'프랑스어: {e2f.get(E)} ')
+    elif menu == '2':
+        print("종료")
+        break
+    else:
+        print('재입력')
+
