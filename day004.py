ffd0ca960068d1e1eb1e9ca91d58b5705ba45ece
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-17 13:33:53 +0900

    v3.8 python day004

--- day004.py
+++ day004.py
@@ -1,14 +1,12 @@
-subjects = ["데이터베이스" ,"C++", "Java", "Python", "Java",'5','9', '리눅스']
-# print(subjects[::-1])
-# subjects[::-1]
-subjects.reverse()
-print(subjects)
-# subjects.remove("Java")
-# del subjects[-2]
-# del subjects[2]
-# subjects.pop(2)
-# subjects.sort(reverse=True)  # desc
-print(subjects)
-copy_subjects = sorted(subjects)
-print(subjects)
-print(copy_subjects)
+import  copy
+# subjects = ['a', 'b', 'c']
+subjects = ['a', ['b', 'c'], 'd']
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+print(subjects, a, b, c, d, e)
+subjects[1][1] = 'x'
+print(subjects, a, b, c, d, e)
+# pythontutor (파이썬 구조 시각화 프로그램)
