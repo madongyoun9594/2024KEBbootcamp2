9f81ea1020cc6ffa66f89827c16e5b56292fd1a9
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-17 17:13:02 +0900

    v0.01 day003

--- 자습.py
+++ 자습.py
@@ -3,10 +3,10 @@ number = int(input('Enter a number: '))
 i = 2
 while i < number:
         if number % i == 0:
-            print(f'{number} is not prime')
+            print(f'The number {number} is not prime')
             break
         i += 1
 if i == number:
     print(f'The number {number} is prime')
-
-
+else:
+    print(f'The number {number} is not prime')
