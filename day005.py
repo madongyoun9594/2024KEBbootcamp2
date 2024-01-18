02fd4ecf01e15879faeda9221b33bd22f227cf07
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-18 14:12:42 +0900

    v4.7 python day005

--- day005.py
+++ day005.py
@@ -1,15 +1,9 @@
-def squares(*n) -> list:
-    """
-    넘겨 받은 수치 데이터들의 거듭제곱 값을 리스트에 담아서 리턴
-    :param n: tuple
-    :return: list
-    """
-    return [pow(i, 2) for i in n]
-    #return n * n
+def out_func(nout):
+    def inner_func():
+        return nout * nout
+    return inner_func
 
-
-def run_function(f, *number) -> list:
-    return f(*number)
-
-print(squares(7, 5, 2))
-print(run_function(squares, 9, 10))
+x = out_func(9)
+print(type(x))
+print(x)
+print(x())
