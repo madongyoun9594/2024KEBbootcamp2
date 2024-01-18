375abe7f33c66da0c110d2a3b8f080b98272d10f
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-18 16:42:14 +0900

    v0.06 practice  day005

--- 자습.py
+++ 자습.py
@@ -1,13 +1,8 @@
-# 8.6 ~ 8.9
-life = dict(aniamls='', plants='', other='')
-animals = dict(cats='Henri', octopi='Grumpy', emus='Lucy')
-print(life.keys())
-print(animals.keys())
-print(animals['cats'])
+def 음식(d):
+    if d == '치킨':
+        return 'c'
+    if d == '밥':
+        return 'r'
 
-
-# 8.10
-numbers = range(10)
-squares = { number : number * number for number in numbers}
-print(squares)
-
+d = input("음식이름: ")
+print(음식(d))
