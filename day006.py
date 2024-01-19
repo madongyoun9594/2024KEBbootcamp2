e3e40ad4504765301709d3629297e7f69a4e168b
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-19 15:10:12 +0900

    v6.2 python day006

--- day006.py
+++ day006.py
@@ -1,21 +1,22 @@
-class Pokemon():
+class FlyingMixin:
+    def fly(self):
+        return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~"
+
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.name}이(가) 수영을 합니다."
+
+class Pokemon:
     def __init__(self, name):
         self.name = name
-    def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) 공격!")
-
-
-class Pikachu(Pokemon):  # is-⍺
-    def __init__(self, name, type):
-        self.type = type
-        self.name = name
-    def attack(self, target):
-        print(f"{self.name}이(가) {target.name}을(를) {self.type} 공격!")
-class Squirtle(Pokemon):
+class Charizard(Pokemon, FlyingMixin):
     pass
 
-p1 = Pikachu("피카츄",'전기')
-p2 = Squirtle("꼬부기")
-p3 = Pokemon("아무개")
-p1.attack(p2)
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
 
+g1 = Gyarados("가랴도스")
+c1 = Charizard("리자몽")
+
+print(c1.fly())
+print(g1.swim())
