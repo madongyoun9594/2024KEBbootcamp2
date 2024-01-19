40b96c59cfecc7bc57692d316d2567578d933b9d
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-19 10:35:55 +0900

    v5.6 python day006

--- day006.py
+++ day006.py
@@ -1,21 +1,28 @@
-# Open closed principle
-def test(f):
-    """
-    데코레이터 함수, 함수 시작하면 start 출력,, 함수 끝나면 end 출력
-    :param f: function
-    :return: closure function
-    """
-    def test_inner(*args, **kwargs):
-        print('start')
-        # result = f(*args, **kwargs)
-        f()
-        print('end')
-        # return result
-    return test_inner
-@test
-def greeting():
-    print('hello')
+def factorial_repetition(n) -> int:
+    result = 1
+    for i in range(2, n + 1):
+        result = result * i
+    return result
 
-greeting()
+def factorial_recursion(n):
+    """
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수 , ㅑㅜㅅ
+    :return: function, int
+    """
+    if n == 1:
+        return n
+    else:
+        return n * factorial_recursion(n-1)
+
+number = int(input("number: "))
+print(factorial_repetition(number))
+print(factorial_recursion(number))
+
+
+
+
+
+
 
 
