a71d664dde8e1c1758b1a8e2c333ef4fb7a1a414
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-19 10:54:51 +0900

    v5.7 python day006

--- day006.py
+++ day006.py
@@ -1,26 +1,25 @@
-def factorial_repetition(n) -> int:
-    result = 1
-    for i in range(2, n + 1):
-        result = result * i
-    return result
+import random
 
-def factorial_recursion(n):
-    """
-    재귀함수를 사용한 팩토리얼 함수
-    :param n: 정수 , ㅑㅜㅅ
-    :return: function, int
-    """
-    if n == 1:
-        return n
-    else:
-        return n * factorial_recursion(n-1)
+# numbers = list()
+# for i in range(5):
+#     numbers.append(random.randint(1,100))
+numbers = [random.randint(1,100) for i in range(5)]
+print(numbers)
 
-number = int(input("number: "))
-print(factorial_repetition(number))
-print(factorial_recursion(number))
+try :
+    pick = int(input(f"Input index (0~{len(numbers)-1}) : "))
+    print(numbers[pick])
+    print(5/0)
+except IndexError as err:
+    print(f"Wrong index number\n{err}")
+except ValueError as err:
+    print(f"Input Only Number~\n{err}")
+except ZeroDivisionError as err:
+    print(f"The denominator cannot be 0.\n{err}")
+except Exception as err:
+    print(f"Error occurs\n{err}")
 
-
-
+# 순서대로 처리되기 때문에 범위가 작은 에러부터 차례대로 코드 작성
 
 
 
