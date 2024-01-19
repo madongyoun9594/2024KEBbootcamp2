3427e8362090d2026f0968b7d96317081ee8a47e
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-19 18:17:14 +0900

    v0.08 practice day006

--- 자습.py
+++ 자습.py
@@ -1,3 +1,18 @@
+class my_profile():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+m1 = my_profile("madongyoun",25)
+
+print(m1.name, m1.age)
+
+
+
+
+
+
+
+
 
 
 
