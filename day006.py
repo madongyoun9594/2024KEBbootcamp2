299e9a38657e8e7fd1b182c14b9fa9df64078dcf
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-19 09:30:49 +0900

    v5.5 assignment 9.4  day006

--- day006.py
+++ day006.py
@@ -1,3 +1,4 @@
+# Open closed principle
 def test(f):
     """
     데코레이터 함수, 함수 시작하면 start 출력,, 함수 끝나면 end 출력
@@ -11,8 +12,10 @@ def test(f):
         print('end')
         # return result
     return test_inner
+@test
 def greeting():
     print('hello')
-t = test(greeting)
-t()
+
+greeting()
+
 
