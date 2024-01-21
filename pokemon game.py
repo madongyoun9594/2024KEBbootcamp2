6c796a800433cc80f99627eed9fdf6c801ea6574
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-22 03:38:08 +0900

    v0.4 pokemongame

--- pokemon game.py
+++ pokemon game.py
@@ -3,7 +3,7 @@ class Pokemon():
     def __init__(self, name, ty, hp, pw):
         self.name = name
         self.ty = ty
-        self.hp = hp
+        self.hp = int(hp)
         self.pw = pw
 
 
@@ -20,29 +20,47 @@ b1 = Bulbasaur("이상해씨",'풀','120','70')
 
 
 class Pokemon_attak(Pokemon):
-    def __init__(self,attack1, attack2):
+    def __init__(self,attack1, attack2, attack1dam, attack2dam):
         self.attack1 = attack1
         self.attack2 = attack2
-pa = Pokemon_attak("전기쇼크", '십만볼트')
-ca = Pokemon_attak("화염방사",'용의파동' )
-ba = Pokemon_attak("덩쿨채찍","씨폭탄")
+        self.attack1dam = int(attack1dam)
+        self.attack2dam = int(attack2dam)
+pa = Pokemon_attak("전기쇼크", '십만볼트','20','35')
+ca = Pokemon_attak("화염방사",'용의파동','35','50')
+ba = Pokemon_attak("덩쿨채찍","씨폭탄",15,25)
 
 
 
 def attackf():
-
-    print()
-    menu = int(input('1) 싸운다 2) 도망간다 :'))
     print()
     if menu == 1:
         print('사용할 기술을 선택하세요')
         print()
         menu_2 = int(input(f'1) {ka.attack1}, 2) {ka.attack2} : '))
+        print()
         if menu_2 == 1:
             print(f'{ka.attack1}!!')
+            return ka.attack1
 
         elif menu_2 == 2:
             print(f'{ka.attack2}!!')
+            return ka.attack2
+def p1enemy_attack():
+    import random
+    number = random.randint(1,2)
+    if number == 1:
+        print(f'{p1.name}이(가) {pa.attack1}을 사용하였다!')
+        k.hp = k.hp - pa.attack1dam
+        print()
+        print(f'{k.name}의 남은 체력: {k.hp}')
+    elif number == 2:
+        print(f'{p1.name}이(가) {pa.attack2}을 사용하였다')
+        k.hp = k.hp - pa.attack2dam
+        print()
+        print(f'{k.name}의 남은 체력: {k.hp}')
+
+
+
 
 
 
@@ -80,11 +98,28 @@ while True:
                     num = random.randint(1,4)
                     if num == 1:
                             print(f'{p1.name}가 나타났다!')
-                            print(0)
-                            print(f'{p1.hp}')
                             print()
-                            while p1.hp == 0:
+                            print(f'{p1.name}의 체력: {p1.hp} {p1.name}의 전투력: {p1.pw}')
+                            print()
+                            menu = int(input('1) 싸운다 2) 도망간다 :'))
+                            while p1.hp > 0 or k.hp > 0:
                                 attackf()
+                                if ka.attack1:
+                                    p1.hp = p1.hp - ka.attack1dam
+                                    print(f'{p1.name}의 남은 체력: {p1.hp}')
+                                    if p1.hp < 0:
+                                        print('승리하였다')
+                                        break
+
+                                    if k.hp < 0:
+                                        print('패배하였다')
+                                        break
+
+
+                                if ka.attack2:
+                                    p1.hp = p1.hp - pa.attack2dam
+                                p1enemy_attack()
+
 
 
                     if num == 2:
@@ -123,6 +158,4 @@ while True:
         break
     else:
         print('choose number 1 or 2')
-
-
 
