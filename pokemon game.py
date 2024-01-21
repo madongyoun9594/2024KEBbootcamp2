ca01a8f318758bfb4f9bc3084da1a119fb67c084
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-22 02:16:48 +0900

    v0.3 pokemongame

--- pokemon game.py
+++ pokemon game.py
@@ -1,11 +1,3 @@
-def pikachu_attak():
-    menu = int(input("1) 전기쇼크 2) 십만볼트 3) 백만볼트 ")
-               if menu == 1:
-                   print('전기쇼크!')
-                    def
-
-
-
 
 class Pokemon():
     def __init__(self, name, ty, hp, pw):
@@ -27,6 +19,30 @@ c1 = Charmander('리자몽','불','140','120')
 b1 = Bulbasaur("이상해씨",'풀','120','70')
 
 
+class Pokemon_attak(Pokemon):
+    def __init__(self,attack1, attack2):
+        self.attack1 = attack1
+        self.attack2 = attack2
+pa = Pokemon_attak("전기쇼크", '십만볼트')
+ca = Pokemon_attak("화염방사",'용의파동' )
+ba = Pokemon_attak("덩쿨채찍","씨폭탄")
+
+
+
+def attackf():
+
+    print()
+    menu = int(input('1) 싸운다 2) 도망간다 :'))
+    print()
+    if menu == 1:
+        print('사용할 기술을 선택하세요')
+        print()
+        menu_2 = int(input(f'1) {ka.attack1}, 2) {ka.attack2} : '))
+        if menu_2 == 1:
+            print(f'{ka.attack1}!!')
+
+        elif menu_2 == 2:
+            print(f'{ka.attack2}!!')
 
 
 
@@ -43,14 +59,19 @@ while True:
             menu_selection = int(input('1) 피카츄 2) 리자몽 3) 이상해씨 : '))
             if menu_selection == 1:
                 k = p1
+                ka = pa
             elif menu_selection == 2:
                 k = c1
+                ka = ca
+
             elif menu_selection == 3:
                 k = b1
+                ka = ba
             print()
             print(f'이름:{k.name}   속성:{k.ty}   체력:{k.hp}   전투력:{k.pw}')
             print()
             print('무엇을 하시겠습니까? ')
+            print()
             while True:
                 menu_ingame = int(input('1) 이동하기 2) 쉬기 3) 나가기 :'))
                 print()
@@ -58,7 +79,27 @@ while True:
                     import random
                     num = random.randint(1,4)
                     if num == 1:
-                            print('피카츄가 나타났다!')
+                            print(f'{p1.name}가 나타났다!')
+                            print(0)
+                            print(f'{p1.hp}')
+                            print()
+                            while p1.hp == 0:
+                                attackf()
+
+
+                    if num == 2:
+                            print(f'{c1.name}가 나타났다!')
+                            print()
+                            attackf()
+                    if num == 3:
+                            print(f'{b1.name}가 나타났다.!')
+                            print()
+                            attackf()
+
+
+
+
+
 
                     if num == 2:
                             print('이상해씨가 나타났다!')
--- 자습.py
+++ 자습.py
@@ -1,30 +1,3 @@
-# class Thing2():
-#     def __init__(self,letters):
-#         self.letters = letters
-#
-# k = Thing2('abc')
-#
-# print(k.letters)
-
-
-class Element:
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.number = number
-        self.symbol =symbol
-    def dump(self):
-        print(f'name: {self.name}, number: {self.number}, symbol: {self.symbol}')
-
-el_dict = dict(name='Hydrogen', symbol='H', number=1)
-
-hydrogen_is = Element(**el_dict)
-
-
-hydrogen_is.dump()
-
-
-
-
 
 
 
