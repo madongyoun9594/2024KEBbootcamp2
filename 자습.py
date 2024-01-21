ab35c67d79ccf762e32ccc1f3afb4d233fdaa469
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-21 23:32:13 +0900

    v0.09 practice day006

--- 자습.py
+++ 자습.py
@@ -1,3 +1,30 @@
+class Thing2():
+    def __init__(self,letters):
+        self.letters = letters
+
+k = Thing2('abc')
+
+print(k.letters)
+
+
+class Element:
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.number = number
+        self.symbol =symbol
+
+
+el_dict = dict(name='Hydrogen', symbol='H', number=1)
+
+hydrogen_is = Element(el_dict['symbol'], el_dict['number'], el_dict['name'])
+
+
+print(hydrogen_is.symbol)
+
+
+
+
+
 
 
 
